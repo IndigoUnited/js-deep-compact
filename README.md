241b042775ebd3c067fe522e0110c9e9d48ad09d
André Cruz <andremiguelcruz@msn.com>
2015-01-24 18:14:07 +0000

    Add browser support.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# deep-compact [![Build Status](https://travis-ci.org/IndigoUnited/node-deep-compact.svg?branch=master)](https://travis-ci.org/IndigoUnited/node-deep-compact)
+# deep-compact [![Build Status](https://travis-ci.org/IndigoUnited/js-deep-compact.svg?branch=master)](https://travis-ci.org/IndigoUnited/js-deep-compact)
 
 Recursively compacts collection values (arrays and objects), removing empty values from your arrays & objects.   
 It will also trim and remove empty strings.
@@ -6,7 +6,12 @@ It will also trim and remove empty strings.
 
 ## Installation
 
-`$ npm install deep-compact`
+`$ npm install deep-compact` - `NPM`   
+`$ bower install deep-compact` - `bower`
+
+The browser file is named `index.umd.js` which supports CommonJS, AMD and globals (`deepCompact`).
+If you want to run this module on old browsers, you must include [es5-shim](https://github.com/es-shims/es5-shim).
+
 
 
 ## Usage
---
+++ index.umd.js
@@ -0,0 +1,112 @@
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.deepCompact=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*jshint eqeqeq:false*/
+
+'use strict';
+
+var isPlainObject = require('is-plain-object');
+var filter = require('deep-filter');
+
+function compact(value) {
+    return filter(value, notEmpty);
+}
+
+function notEmpty(value, prop, subject) {
+    var key;
+
+    if (Array.isArray(value)) {
+        return value.length > 0;
+    }
+
+    if (isPlainObject(value)) {
+        for (key in value) {
+            return true;
+        }
+
+        return false;
+    }
+
+    if (typeof value === 'string') {
+        value = subject[prop] = value.trim();
+
+        return value.length > 0;
+    }
+
+    return value != null;
+}
+
+module.exports = compact;
+
+},{"deep-filter":2,"is-plain-object":3}],2:[function(require,module,exports){
+'use strict';
+
+var isPlainObject = require('is-plain-object');
+
+function filter(value, fn) {
+    if (Array.isArray(value)) {
+        return filterArray(value, fn);
+    } else if (isPlainObject(value)) {
+        return filterObject(value, fn);
+    }
+
+    return value;
+}
+
+function filterObject(obj, fn) {
+    var newObj = {};
+    var key;
+    var value;
+
+    for (key in obj) {
+        value = filter(obj[key], fn);
+
+        if (fn.call(obj, value, key, obj)) {
+            if (value !== obj[key] && !isCollection(value)) {
+                value = obj[key];
+            }
+
+            newObj[key] = value;
+        }
+    }
+
+    return newObj;
+}
+
+function filterArray(array, fn) {
+    var filtered = [];
+
+    array.forEach(function (value, index, array) {
+        value = filter(value, fn);
+
+        if (fn.call(array, value, index, array)) {
+            if (value !== array[index] && !isCollection(value)) {
+                value = array[index];
+            }
+
+            filtered.push(value);
+        }
+    });
+
+    return filtered;
+}
+
+function isCollection(value) {
+    return Array.isArray(value) || isPlainObject(value);
+}
+
+module.exports = filter;
+
+},{"is-plain-object":3}],3:[function(require,module,exports){
+/*!
+ * is-plain-object <https://github.com/jonschlinkert/is-plain-object>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT License
+ */
+
+'use strict';
+
+module.exports = function isPlainObject(o) {
+  return !!o && typeof o === 'object' && o.constructor === Object;
+};
+},{}]},{},[1])(1)
+});
